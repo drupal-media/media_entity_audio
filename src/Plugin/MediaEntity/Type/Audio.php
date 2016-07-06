e171b42c510e16af54ed89048715b28a693c68c4
Arit Kumar Nath <arit.nath@dcplkolkata.com>
2016-07-06 13:23:08 +0530

    Add "Type provider configuration"

--- src/Plugin/MediaEntity/Type/Audio.php
+++ src/Plugin/MediaEntity/Type/Audio.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace Drupal\media_entity_audio\Plugin\MediaEntity\Type;
-
-use Drupal\media_entity\MediaBundleInterface;
 use Drupal\media_entity\MediaInterface;
 use Drupal\media_entity\MediaTypeBase;
-
+use Drupal\Core\Form\FormStateInterface;
 /**
  * Provides media type plugin for Audio.
  *
@@ -16,63 +13,55 @@ use Drupal\media_entity\MediaTypeBase;
  * )
  */
 class Audio extends MediaTypeBase {
-
   /**
    * {@inheritdoc}
    */
-  public function settingsForm(MediaBundleInterface $bundle) {
-    $form = array();
-
-    $options = array();
-    $allowed_field_types = array('file');
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
+    $bundle = $form_state->getFormObject()->getEntity();
+    $options = [];
+    $allowed_field_types = ['file'];
     foreach ($this->entityFieldManager->getFieldDefinitions('media', $bundle->id()) as $field_name => $field) {
       if (in_array($field->getType(), $allowed_field_types) && !$field->getFieldStorageDefinition()->isBaseField()) {
         $options[$field_name] = $field->getLabel();
       }
     }
-    $form['source_field'] = array(
+    $form['source_field'] = [
       '#type' => 'select',
       '#title' => t('Field with source information'),
-      '#description' => t('Field on media entity that stores Audio file.'),
+      '#description' => t('Field on media entity that stores Audio file. You can create a bundle without selecting a value for this dropdown initially. This dropdown can be populated after adding fields to the bundle.'),
       '#default_value' => empty($this->configuration['source_field']) ? NULL : $this->configuration['source_field'],
       '#options' => $options,
-    );
-
+    ];
     return $form;
   }
-
   /**
    * {@inheritdoc}
    */
   public function providedFields() {
     return [];
   }
-
   /**
    * {@inheritdoc}
    */
-  public function getField(MediaInterface $media , $name) {
+  public function getField(MediaInterface $media, $name) {
     return FALSE;
   }
-
   /**
    * {@inheritdoc}
    */
   public function getDefaultThumbnail() {
     return $this->config->get('icon_base') . '/image.png';
   }
-
   /**
    * {@inheritdoc}
    */
   public function thumbnail(MediaInterface $media) {
     $source_field = $this->configuration['source_field'];
-
     /** @var \Drupal\file\FileInterface $file */
     if ($file = $this->entityTypeManager->getStorage('file')->load($media->{$source_field}->target_id)) {
       return $this->config->get('icon_base') . '/image.png';
     }
-
     return $this->getDefaultThumbnail();
   }
 

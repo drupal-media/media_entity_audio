32af1d4c15305596068c93e2ad17ad96c7b808ce
slashrsm <slashrsm@744628.no-reply.drupal.org>
2016-07-22 22:45:14 +0200

    Issue #2767869 by slashrsm: Chase changes in entity_browser

--- src/Plugin/EntityBrowser/Widget/Upload.php
+++ src/Plugin/EntityBrowser/Widget/Upload.php
@@ -4,6 +4,7 @@ namespace Drupal\media_entity_audio\Plugin\EntityBrowser\Widget;
 
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\entity_browser\Plugin\EntityBrowser\Widget\Upload as FileUpload;
+use Drupal\media_entity\MediaInterface;
 
 /**
  * Uses upload to create media entity audios.
@@ -31,7 +32,7 @@ class Upload extends FileUpload {
    */
   public function getForm(array &$original_form, FormStateInterface $form_state, array $aditional_widget_parameters) {
     /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
-    if (!$this->configuration['media bundle'] || !($bundle = $this->entityManager->getStorage('media_bundle')->load($this->configuration['media bundle']))) {
+    if (!$this->configuration['media bundle'] || !($bundle = $this->entityTypeManager->getStorage('media_bundle')->load($this->configuration['media bundle']))) {
       return ['#markup' => t('The media bundle is not configured correctly.')];
     }
 
@@ -48,26 +49,41 @@ class Upload extends FileUpload {
   /**
    * {@inheritdoc}
    */
-  public function submit(array &$element, array &$form, FormStateInterface $form_state) {
+  protected function prepareEntities(array $form, FormStateInterface $form_state) {
+    $files = parent::prepareEntities($form, $form_state);
+
     /** @var \Drupal\media_entity\MediaBundleInterface $bundle */
-    $bundle = $this->entityManager
+    $bundle = $this->entityTypeManager
       ->getStorage('media_bundle')
-      ->load($this->configuration['media bundle']);
-    $files = $this->extractFiles($form_state);
+      ->load($this->configuration['media_bundle']);
 
     $audios = [];
     foreach ($files as $file) {
-      /** @var \Drupal\media_entity\MediaInterface $audio */
-      $audio = $this->entityManager->getStorage('media')->create([
+      /** @var \Drupal\media_entity\MediaInterface $image */
+      $image = $this->entityTypeManager->getStorage('media')->create([
         'bundle' => $bundle->id(),
         $bundle->getTypeConfiguration()['source_field'] => $file,
       ]);
-      $audio->save();
-      $audios[] = $audio;
+      $audios[] = $image;
     }
 
-    $this->selectEntities($audios, $form_state);
-    $this->clearFormValues($element, $form_state);
+    return $audios;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submit(array &$element, array &$form, FormStateInterface $form_state) {
+    if (!empty($form_state->getTriggeringElement()['#eb_widget_main_submit'])) {
+      $images = $this->prepareEntities($form, $form_state);
+      array_walk(
+        $images,
+        function (MediaInterface $media) { $media->save(); }
+      );
+
+      $this->selectEntities($images, $form_state);
+      $this->clearFormValues($element, $form_state);
+    }
   }
 
 }
